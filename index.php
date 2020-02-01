b3862e2849b90812a89b8604494881cc6bef53da
Cristobal De Jesus Silva Diaz <cristobal.silva@SofttekClients.com>
2020-02-01 09:46:16 -0600

    Logo

--- index.php
+++ index.php
@@ -52,10 +52,7 @@
   ?>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-     <a class="navbar-brand" href="#"><img src="DevOps.png" style="max-width:70px"/></a>
-     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-icon"></span>
-     </button>
+     <a class="navbar-brand" href="index.php"><img src="DevOps.png" style="max-width:70px"/></a>
      <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
@@ -70,6 +67,9 @@
           <button data-toggle="tooltip" <?php if(isset($_GET['edit'])){echo "title='Update User'";}else{echo "title='Add User'";} ?> class="btn btn-outline-success my-2 my-sm-0" type="submit">
             <?php if(isset($_GET['edit'])){echo "<i class='fa fa-refresh'></i>  Update";}else{echo "<i class='fa fa-plus'></i> Add User";}?></button>
         </form>
+       </div>
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <a class="navbar-brand" href="index.php"><img src="Softtek.png" style="max-width:70px"/></a>
        </div>
     </nav>
     <div class="container">
