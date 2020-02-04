46bab9b4e6bb844f306b3ee580244cf997980c4c
Cristobal De Jesus Silva Diaz <cristobal.silva@SofttekClients.com>
2020-02-04 11:18:22 -0600

    trash

--- index.php
+++ index.php
@@ -132,8 +132,9 @@
                             echo "<td><center>
                                   <a target='_blank' href='ftp://$ftp_user_name:$ftp_user_pass@$ftp_server/".$file."'
                                     data-toggle='tooltip' title='Download ".$file."'>
-                                    <i class='fa fa-file-o'></i><br>".$file."<br>
-                                    <i class='fa fa-trash btn btn-danger'></i></a></center></td>";
+                                    <i class='fa fa-file-o'></i><br>".$file."</a><br>
+                                    <a href='index.php?dropfile=".$file."'>
+                                    <i class='fa fa-trash btn btn-outline-danger'></i></a></center></td>";
                             if ($i == 3) { echo "</tr><tr>"; }
                         }
                       }
@@ -154,11 +155,12 @@
                     $uploads = ftp_nlist($conn_id, "");
                       foreach ($uploads as &$file) {
                         $i++;
-                            echo "<td><center>
-                                  <a target='_blank' href='ftp://$ftp_user_name:$ftp_user_pass@$ftp_server/".$file."'
-                                    data-toggle='tooltip' title='Download ".$file."'>
-                                    <i class='fa fa-file-o'></i><br>".$file."<br>
-                                    <i class='fa fa-trash btn btn-danger'></i></a></center></td>";
+                        echo "<td><center>
+                              <a target='_blank' href='ftp://$ftp_user_name:$ftp_user_pass@$ftp_server/".$file."'
+                                data-toggle='tooltip' title='Download ".$file."'>
+                                <i class='fa fa-file-o'></i><br>".$file."</a><br>
+                                <a href='index.php?dropfile=uploaded/".$file."'>
+                                <i class='fa fa-trash btn btn-outline-danger'></i></a></center></td>";
                             if ($i == 3) { echo "</tr><tr>"; }
                       }
                    ?>
